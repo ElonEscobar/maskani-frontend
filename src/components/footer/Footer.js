343c996e20295eb22378df56259c61c63e919696
danielgathemia <danielgathemia01@gmail.com>
2023-02-26 00:20:06 +0300

    footer update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+  
   return (
-    <div>Footer</div>
-  )
+    <div className="footer">
+      <nav>
+        <ul>
+        <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <p>&copy; {currentYear} My Website. All rights reserved.</p>
+
+  </div>
+);
 }
+
 
 export default Footer
