5fce6d7a6bc832d33e4d95e7c8118dbb2f8c8349
Dan Muendo <danwalker254254@gmaail.com>
2023-03-01 15:59:12 +0300

    initialized property cards

--- src/components/property/Property.js
+++ src/components/property/Property.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import './property.css'
 
 function Property() {
   return (
-    <div>Property</div>
+    <div className='flats'>
+      <div className='main-card'>
+        <div className='card-img'>
+          <div className='type'>
+            <span>For Sale</span>
+          </div>
+          <div className='m-button'>
+            <button>More details</button>
+          </div>
+        </div>
+        <div className='details'>
+          <div>
+            <span>Flat Name</span>
+            <i class='fas fa-map-marker-alt'></i>
+          </div>
+          <div className='price'>5999</div>
+        </div>
+      </div>
+      <div className='main-card'>
+        <div className='card-img'>
+          <div className='type'>
+            <span>For Sale</span>
+          </div>
+          <div className='m-button'>
+            <button>More details</button>
+          </div>
+        </div>
+        <div className='details'>
+          <div>
+            <span>Flat Name</span>
+            <span>location</span>
+          </div>
+          <div className='price'>4999</div>
+        </div>
+      </div>
+     
+    </div>
   )
 }
 
