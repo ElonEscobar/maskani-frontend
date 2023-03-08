9f74ac237bd7dc335966bc699444946570443edd
Dan Muendo <danwalker254254@gmaail.com>
2023-03-08 21:26:30 +0300

    home intro animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
       <div className="home">
         <img src="https://i.ibb.co/TtM2ckm/home-background.jpg" alt="home-background" border="0"></img>
         <div className='home-intro'>
-          <div className='title'>WELCOME TO MASKANI KENYA</div>
+          <div className='title'>WELCOME TO <div>MASKANI KENYA</div></div>
           <div className='detail'>FIND YOUR DREAM HOME WITH US</div>
           <p>Maskani Kenya is the trusted resource for home buyers, sellers, and dreamers offering the most comprehensive source of for-sale or rent properties</p>
           
