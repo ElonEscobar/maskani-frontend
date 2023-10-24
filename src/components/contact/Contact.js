5137a96015d0761b0d225e662872905fcd2195ff
Leon simintei <mallfs@icloud.com>
2023-10-24 10:56:32 +0300

    contactjs

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import './contact.css'
+import emailjs from '@emailjs/browser';
+import {useRef} from 'react'
 
 function Contact() {
+
+  const sendEmail = (e) => {
+    e.prventDefault();
+    emailjs.sendForm('service_8c1mcvk', 'template_7lo643t', form.current, '1GnPFN6kcleLf31Gc')
+    e.target.reset()
+  }
+
+  const form = useRef();
+
   return (
-    <div>Contact</div>
+   <section>
+    <div>
+      <form ref={form} onSubmit={sendEmail}>
+        <input type='text' name='name' placeholder='Your full name' required/>
+        <input type='email' name='email' placeholder='Your email address' required/>
+        <textarea name='message' rows="10" placeholder='Message...'></textarea>
+        <button className='btn'>Send message</button>
+      </form>
+    </div>
+   </section>
   )
 }
 
